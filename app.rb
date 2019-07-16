f67d00644809a8df8cf0d112731cb743f30015ea
github username <github email address>
2019-07-16 14:29:13 -0500

    not done yet

--- app.rb
+++ app.rb
@@ -3,4 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
-end
+
+  get '/reversename/:name'
+    params[:name].reverse
+  end 
+
+  get '/square/:number'
+   num = params[:number]
+   num.to_i **2
+  end 
+end 
